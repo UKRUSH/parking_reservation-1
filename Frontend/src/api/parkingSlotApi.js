2a78aa2751fd983d998dfb4e1874bebf17ed2073
Chathumi <it23206946@my.sliit.lk>
2026-04-23 22:49:50 +0530

    feat(api): extend parkingSlotApi with CRUD and add helmetApi client

--- Frontend/src/api/parkingSlotApi.js
+++ Frontend/src/api/parkingSlotApi.js
@@ -1,6 +1,16 @@
 import axiosInstance from './axiosInstance'
 
 export const parkingSlotApi = {
-  getSlots: (type, startTime, endTime) =>
+  getSlots:     (type, startTime, endTime) =>
     axiosInstance.get('/parking-slots', { params: { type, startTime, endTime } }),
+
+  getById:      (id)          => axiosInstance.get(`/parking-slots/${id}`),
+
+  create:       (data)        => axiosInstance.post('/parking-slots', data),
+
+  update:       (id, data)    => axiosInstance.put(`/parking-slots/${id}`, data),
+
+  updateStatus: (id, status)  => axiosInstance.patch(`/parking-slots/${id}/status`, { status }),
+
+  delete:       (id)          => axiosInstance.delete(`/parking-slots/${id}`),
 }
